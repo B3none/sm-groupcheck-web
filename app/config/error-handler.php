d0d2e0d81ad57d246c0b9c668d0b7ed7e427995f
b3none <ablackham2000@gmail.com>
2018-07-16 18:38:06 +0100

    Altered error handler to handle all types of errors.

--- app/config/error-handler.php
+++ app/config/error-handler.php
@@ -1,14 +1,12 @@
 <?php
 
 $app->error(function (\Exception $e, $code) use ($app) {
-    if ($code == 404) {
-        return $app->json(['status' => 404, 'message' => $e->getMessage()]);
-    }
+    return $app->json(['status' => $code, 'message' => $e->getMessage()]);
 
-    if ($app['debug']) {
-        // In debug mode we want to get the regular error message
-        return;
-    }
-
-    return new \Symfony\Component\HttpFoundation\Response('We are sorry, but something went terribly wrong.');
+//    if ($app['debug']) {
+//        // In debug mode we want to get the regular error message
+//        return;
+//    }
+//
+//    return new \Symfony\Component\HttpFoundation\Response('We are sorry, but something went terribly wrong.');
 });
